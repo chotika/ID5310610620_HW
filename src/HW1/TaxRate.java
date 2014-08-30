0871bb4cdd23854f1d6aea3a2e29d94729f4465c
MY-SNSD <MY-SNSD@MY-SNSD-PC>
2014-08-30 22:17:07 +0700

    test push on github

---
+++ src/HW1/TaxRate.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HW1;
+
+/**
+ *
+ * @author MY-SNSD
+ */
+public class TaxRate {
+
+    public static void main(String[] args) {
+        
+    }
+}
